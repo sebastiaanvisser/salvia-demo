6e75e83e59566e96740070cc2d66509ad4fdbc77
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-08-08 18:06:27 +0200

    Demo now uses dummy sessions. SendFile uses raw socket action.

--- src/Demo.hs
+++ src/Demo.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Concurrent.STM
 import Control.Monad.Trans
 import Network.Salvia.Handler.ExtendedFileSystem
 import Network.Salvia.Handlers
@@ -10,15 +11,19 @@ import Network.Socket hiding (Socket)
 
 main :: IO ()
 main =
-  do conf <- defaultConfig
-     addr <- inet_addr "127.0.0.1"
+  do conf     <- defaultConfig
+     addr     <- inet_addr "127.0.0.1"
+     count    <- atomically (newTVar 0)
+     sessions <- mkSessions :: IO (Sessions ())
      putStrLn "started"
      start 
        (conf { listenAddr = addr, listenPort = 8080 })
-       (hDefaultEnv myHandler)
+       (hSessionEnv count sessions myHandler)
 
 -- Serve the current directory.
 
-myHandler :: (MonadIO m, Request m, Response m, Send m, Socket m) => m ()
-myHandler = hExtendedFileSystem "."
+myHandler
+  :: (MonadIO m, Request m, Response m, Send m, Socket m)
+  => TSession () -> m ()
+myHandler _ = hExtendedFileSystem "."
 
--- src/Network/Salvia/Handler/SendFile.hs
+++ src/Network/Salvia/Handler/SendFile.hs
@@ -2,26 +2,21 @@ module Network.Salvia.Handler.SendFile (
     hSendFileResource
   ) where
 
-import Data.Record.Label
 import Control.Monad.Trans
-import Network.Protocol.Uri
+import Data.Record.Label
 import Network.Protocol.Http
-import Network.Protocol.Mime
 import Network.Salvia.Handlers
 import Network.Salvia.Httpd
 import Network.Socket.SendFile
 import System.IO
 
 hSendFileResource :: (Socket m, Send m, MonadIO m, Response m) => String -> m ()
-hSendFileResource file = do
-  let m = maybe defaultMime id $ (either (const Nothing) Just (parseURI file) >>= mimetype . lget path)
-  sck <- rawSock
-  hSafeIO (openBinaryFile file ReadMode) $ \fd ->
-    hSafeIO (hFileSize fd)               $ \fs ->
-      do response $
-           do setM status OK
-              setM contentType (m, Just utf8)
-              setM contentLength (Just fs)
-         hSafeIO (sendFile' sck fd 0 fs) $
-           return
+hSendFileResource f =
+  hSafeIO (openBinaryFile f ReadMode) $ \fd ->
+  hSafeIO (hFileSize fd)                 $ \fs ->
+    do response $
+         do setM status OK
+            setM contentType (fileMime f, Just utf8)
+            setM contentLength (Just fs)
+       raw (\(s, _) -> sendFile' s fd 0 fs)
 
