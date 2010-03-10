54930aa6eb32bc5d0fde18a36601ba8594ab55b6
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-03-11 00:40:49 +0100

    C10k server has own handele type that does not allow tvar payloads.

--- src/Network/Salvia/Impl/C10k.hs
+++ src/Network/Salvia/Impl/C10k.hs
@@ -1,24 +1,70 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Network.Salvia.Impl.C10k (start) where
 
+import Data.Monoid
 import Control.Monad.State
+import Control.Applicative
 import Network.C10kServer
-import Network.TCPInfo
-import Network.Salvia.Impl.Config
+import Network.Protocol.Http hiding (accept, hostname)
 import Network.Salvia.Impl.Context 
+import Network.Salvia.Interface
 import Network.Salvia.Impl.Handler
 import Network.Socket
+import Network.TCPInfo
 
-start :: String -> String -> C10kConfig -> Handler Config p () -> p -> IO ()
-start host admin conf handler payload =
+newtype C10kHandler p a = C10kHandler (Handler p a)
+  deriving
+  ( BodyM Request 
+  , Alternative 
+  , Applicative 
+  , BodyM Response 
+  , ClientAddressM 
+  , FlushM Request 
+  , FlushM Response 
+  , Functor 
+  , HttpM Request 
+  , HttpM Response 
+  , Monad
+  , MonadIO 
+  , MonadPlus 
+  , Monoid
+  , QueueM 
+  , RawHttpM Request 
+  , RawHttpM Response 
+  , SendM 
+  , ForkM IO
+  , ServerAddressM 
+  , ServerM 
+  , SockM 
+  )
+
+runC10kHandler :: C10kHandler p a -> Context p -> IO (a, Context p)
+runC10kHandler (C10kHandler h) = runHandler h
+
+start :: String -> String -> C10kConfig -> C10kHandler p () -> p -> IO ()
+start hst admn conf handler pyld =
    do putStrLn ("Starting listening server on: 0.0.0.0:" ++ portName conf)
-      runC10kServer
-        (\s h i ->
-          do my   <- (addrAddress . head) `fmap` getAddrInfo Nothing (Just (myAddr i))   (Just (myPort i))
-             peer <- (addrAddress . head) `fmap` getAddrInfo Nothing (Just (peerAddr i)) (Just (peerPort i))
-             evalStateT
-               (unHandler handler)
-               (mkContext (Config host admin [] 16) payload peer my s h)
-        ) conf
+      flip runC10kServer conf $ \s h i ->
+        do let addrPort a p = addrAddress . head <$> getAddrInfo Nothing (Just a) (Just p)
+           my   <- addrPort (myAddr   i) (myPort   i)
+           peer <- addrPort (peerAddr i) (peerPort i)
+           _ <- runC10kHandler handler
+             Context
+             { _cServerHost  = hst
+             , _cAdminMail   = admn
+             , _cListenOn    = [{-todo-}]
+             , _cPayload     = pyld
+             , _cRequest     = emptyRequest
+             , _cResponse    = emptyResponse
+             , _cRawRequest  = emptyRequest
+             , _cRawResponse = emptyResponse
+             , _cSocket      = s
+             , _cHandle      = h
+             , _cClientAddr  = peer
+             , _cServerAddr  = my
+             , _cQueue       = []
+             }
+           return ()
 
 {-
 
