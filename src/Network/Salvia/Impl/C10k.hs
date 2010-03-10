697ed2e0cf0f0fe19118e06d148d85b23185c677
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-03-10 17:23:44 +0100

    Added a new salvia server implementation using C10k.

---
+++ src/Network/Salvia/Impl/C10k.hs
@@ -0,0 +1,43 @@
+module Network.Salvia.Impl.C10k (server) where
+
+import Control.Monad.State
+import Network.C10kServer
+import Network.TCPInfo
+import Network.Salvia.Impl.Config
+import Network.Salvia.Impl.Context 
+import Network.Salvia.Impl.Handler
+import Network.Socket
+
+server :: String -> String -> C10kConfig -> Handler Config p () -> p -> IO ()
+server host admin conf handler payload =
+   do runC10kServer
+        (\s h i ->
+          do my   <- (addrAddress . head) `fmap` getAddrInfo Nothing (Just (myAddr i))   (Just (myPort i))
+             peer <- (addrAddress . head) `fmap` getAddrInfo Nothing (Just (peerAddr i)) (Just (peerPort i))
+             evalStateT
+               (unHandler handler)
+               (mkContext (Config host admin [] 16) payload peer my s h)
+        ) conf
+
+{-
+
+test :: IO ()
+test = 
+  server "localhost" "root@localhost"
+  C10kConfig
+    { initHook               = return ()
+    , exitHook               = \s -> putStrLn ("C10k error:" ++ s)
+    , parentStartedHook      = return ()
+    , startedHook            = return ()
+    , sleepTimer             = 10
+    , preforkProcessNumber   = 10
+    , threadNumberPerProcess = 100
+    , portName               = "8080"
+    , pidFile                = "pid"
+    , user                   = "sebas"
+    , group                  = "wheel"
+    }
+  (hDefaultEnv (hExtendedFileSystem "." >> hColorLog stdout))
+  ()
+
+-}
