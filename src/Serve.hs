517f730a5da194302701502b2770b0f468843267
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-03-11 17:13:58 +0100

    The simple server example now uses the C10k backend.

--- src/Serve.hs
+++ src/Serve.hs
@@ -1,11 +1,28 @@
 module Main where
 
-import Network.Salvia
-import Network.Salvia.Handler.ExtendedFileSystem
+import Network.C10kServer
 import Network.Salvia.Handler.ColorLog
+import Network.Salvia.Handler.ExtendedFileSystem
+import Network.Salvia.Handlers
+import Network.Salvia.Impl.C10k
 import System.IO
-import qualified Network.Salvia.Impl.Server as Server
 
 main :: IO ()
-main = Server.start defaultConfig serve ()
-  where serve = hDefaultEnv (hExtendedFileSystem "." >> hColorLog stdout)
+main = 
+  start "localhost" "root@localhost"
+  C10kConfig
+    { initHook               = return ()
+    , exitHook               = \s -> putStrLn ("C10kServer error:" ++ s)
+    , parentStartedHook      = return ()
+    , startedHook            = return ()
+    , sleepTimer             = 10
+    , preforkProcessNumber   = 200
+    , threadNumberPerProcess = 200
+    , portName               = "8080"
+    , pidFile                = "pid"
+    , user                   = "sebas"
+    , group                  = "wheel"
+    }
+  (hDefaultEnv (hExtendedFileSystem ".")) --  >> hColorLog stdout))
+  ()
+
