922d6ca8279aa24d1bc5ca6fbbf70fc76e00c939
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-03-12 14:24:26 +0100

    There are now two extended filesystem handlers, one with and one without sendfile.

--- src/Network/Salvia/Handler/ExtendedFileSystem.hs
+++ src/Network/Salvia/Handler/ExtendedFileSystem.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
-module Network.Salvia.Handler.ExtendedFileSystem (hExtendedFileSystem) where
+module Network.Salvia.Handler.ExtendedFileSystem
+( hExtendedFileSystemSendFile
+, hExtendedFileSystem
+) where
 
 import Control.Applicative
 import Control.Monad.Trans
@@ -9,14 +12,25 @@ import Network.Salvia.Handler.CleverCSS
 import Network.Salvia.Handler.HsColour
 import Network.Salvia.Handler.SendFile
 
+hExtendedFileSystemSendFile
+  :: (MonadIO m, HttpM' m, SocketQueueM m, SendM m, BodyM Request m, Alternative m)
+  => String -> m ()
+hExtendedFileSystemSendFile dir = hFileTypeDispatcher hDirectoryResource f dir
+  where
+  f file = hHighlightHaskell (hHsColour file)
+         . hWithDir dir
+         . hFilterCSS hCleverCSS
+         . hSendFileResource 
+         $ file
+
 hExtendedFileSystem
-  :: (MonadIO m, HttpM' m, QueueM m, SendM m, BodyM Request m, Alternative m)
+  :: (MonadIO m, HttpM' m, SendM m, BodyM Request m, Alternative m)
   => String -> m ()
 hExtendedFileSystem dir = hFileTypeDispatcher hDirectoryResource f dir
   where
   f file = hHighlightHaskell (hHsColour file)
          . hWithDir dir
          . hFilterCSS hCleverCSS
-         . hSendFileResource 
+         . hFileResource 
          $ file
 
